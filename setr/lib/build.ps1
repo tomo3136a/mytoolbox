5a6edccf702f0faf02e41192c274c14a1c63353a
tomo3136a <tomo3136a@gmail.com>
2025-10-19 06:46:06 +0900

    [setr] change submodule to sub folder

---
+++ setr/lib/build.ps1
@@ -0,0 +1,31 @@
+﻿#builder
+#
+param($OutputPath = "./bin", [switch]$pass)
+
+$AppName = Split-Path "." -Leaf
+$AppExts = ".exe"
+
+$Path = "src/*.cs"
+$ReferencedAssemblies = "System.Drawing", "System.Windows.Forms", `
+  "System.Xml", "System.Xml.Linq"
+
+Write-Host "build start." -ForegroundColor Yellow
+
+if (-not (Test-Path $OutputPath)) {
+  New-Item -Path $OutputPath -ItemType Directory | Out-Null
+  Write-Output "make directory."
+}
+$OutputAssembly = Join-Path (Resolve-Path -Path $OutputPath) $AppName$AppExts
+
+Write-Output "build program:  $AppName$AppExts"
+Write-Output "    Path:       $Path"
+Write-Output "    Output:     $OutputAssembly"
+Write-Output "    References: $ReferencedAssemblies"
+Add-Type -Path $Path -OutputType ConsoleApplication `
+  -OutputAssembly $OutputAssembly `
+  -ReferencedAssemblies $ReferencedAssemblies
+
+Copy-Item -Force lib/install.cmd $OutputPath
+
+Write-Host "build completed." -ForegroundColor Yellow
+if (-not $pass) { $host.UI.RawUI.ReadKey() | Out-Null }
