b3a6581df7dacfd16a0f1f87c135e771e0782a0a
tomo3136a <tomo3136a@gmail.com>
2025-11-28 07:43:37 +0900

    [autocopy] modified install batch

--- autocopy/lib/install.cmd
+++ autocopy/lib/install.cmd
@@ -1,8 +1,18 @@
 @echo off
 pushd %~dp0
+set INST=c:\opt\bin
+set APP=autocopy
+
 echo installing...
-if exist ..\bin\autocopy.exe (
-  ..\bin\autocopy.exe -u
+if exist ..\bin\%APP%%.exe (
+  if not exist %INST% mkdir %INST% 
+  copy ..\bin\%APP%.exe %INST%
+)
+set LIB=%INST%\..\lib
+if exist ..\lib\install_task.cmd (
+  if not exist %LIB% mkdir %LIB% 
+  copy ..\lib\install_task.cmd %LIB%\install_task_%APP%.cmd
+  copy ..\lib\install_task.ps1 %LIB%\install_task_%APP%.ps1
 )
 popd
 pause
